b135d12b4d75e2e9e74b71578e7fc5661f61f8b2
KhafiaAyyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-07-16 23:13:23 +0530

    Create Is prime.js

---
+++ Is prime.js
@@ -0,0 +1,47 @@
+/*  IsPrime
+
+Take as input a number n. Determine whether it is prime or not. If it is prime, 
+print "Prime" otherwise print "Not Prime".
+
+Input Format
+Integer
+
+Constraints
+n <= 10 ^ 9
+
+Output Format
+Prime Not Prime
+
+Sample Input 0
+7
+
+Sample Output 0
+Prime
+
+*/
+function processData(input) {
+    //Enter your code here\
+      let isPrime = true;
+    for(let div = 2; div * div<=input;div++){
+        if(input % div == 0){
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime ==  true){
+        console.log("Prime");
+    }else{
+        console.log("Not Prime")
+    }
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
